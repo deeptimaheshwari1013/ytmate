f06e58fc384d9704613e597cf17e96f8337a821b
deepti <deeptimaheshwari1013@gmail.com>
2026-06-05 21:27:46 +0530

    added ffmpeg installation

--- src/setup.py
+++ src/setup.py
@@ -15,6 +15,36 @@ def main(env_dir="venv", req_file="requirements.txt"):
     if os.path.exists(req_file):
         print(f"Installing requirements from {req_file}...")
         sbp.check_call([python_exe, "-m", "pip", "install", "-r", req_file])
+        # for mac os
+        if sys.platform == "darwin":
+            #installing homebrew 
+            try:
+                brew_command = '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+                os.system(brew_command)
+                
+                # now with this im installing ffmpeg
+                sbp.check_call(["brew","install","ffmpeg"])
+                print("worked!")
+            except:
+                print("unable to instal ffmpeg on macos")
+                
+        elif sys.platform == "win32":
+            try:    
+                winget_command = 'Install-Script -Name winget-install -Force; winget-install'
+                os.system(winget_command)
+                # now with this installing ffmpeg
+                sbp.check_call(["powershell", "-Command","Install-Script -Name winget-install -Force; winget-install"])
+                print("workeddd!")
+            except:
+                print("unable to install ffmpeg on windows")
+                
+        else:
+            try:
+                # apt is pre-installed in ubuntu systems 
+                sbp.check_call(["sudo","apt","install","ffmpeg"])
+                print("worked")
+            except:
+                print("unable to install ffmpeg on linux(ubuntu)")
     else:
         print(f"No {req_file} found. Skipping installation.")
 
